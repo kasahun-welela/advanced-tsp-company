058dfe8410f4d740090e924e578ce8e06ac15b0e
kasahun-welela <kasahunwelela1@gmail.com>
2025-07-22 19:14:59 +0300

    update nav bar

--- components/layouts/NavBar.tsx
+++ components/layouts/NavBar.tsx
@@ -18,6 +18,10 @@ const NavBar = () => {
   const [servicesOpen, setServicesOpen] = useState(false);
   const pathname = usePathname();
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   const navLinks = [
     { name: "Home", href: "/" },
     { name: "About", href: "/about" },
@@ -61,10 +65,14 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="w-full z-50 relative">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between py-4 items-center ">
-          <div className="flex-shrink-0 text-xl font-bold">TSP Company</div>
+    <nav className="w-full  ">
+      {/* Desktop Navigation */}
+      <div className="hidden md:block fixed top-0 left-0 right-0 bg-white dark:bg-slate-800  z-50 tracking-wide">
+        <div className="flex justify-between py-4 items-center container mx-auto px-4 sm:px-6 lg:px-8  ">
+          <Link href="/" className="text-xl font-bold">
+            TSP Company
+          </Link>
+
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center flex-1 justify-end">
             <NavigationMenu>
@@ -72,14 +80,14 @@ const NavBar = () => {
                 {navLinks.map((link) =>
                   !link.submenu ? (
                     <NavigationMenuItem key={link.name}>
-                      <NavigationMenuLink
+                      <Link
                         href={link.href}
                         className={`px-4 py-2 text-sm font-medium hover:text-primary transition-colors ${
                           pathname === link.href ? "text-primary" : ""
                         }`}
                       >
                         {link.name}
-                      </NavigationMenuLink>
+                      </Link>
                     </NavigationMenuItem>
                   ) : (
                     <NavigationMenuItem key={link.name}>
@@ -112,7 +120,7 @@ const NavBar = () => {
             </NavigationMenu>
           </div>
           {/* Mobile Navigation */}
-          <div className="md:hidden flex items-center">
+          {/* <div className="md:hidden flex items-center">
             <button onClick={() => setMenuOpen(!menuOpen)}>
               {menuOpen ? (
                 <X className="text-2xl" />
@@ -120,90 +128,103 @@ const NavBar = () => {
                 <Menu className="text-2xl" />
               )}
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
+      {/* Mobile Navigation */}
+      <div className=" bg-white dark:bg-slate-800  flex justify-between items-center px-4 md:hidden py-5 fixed top-0 left-0 right-0  z-50 transition-all duration-300">
+        <Link href="/">
+          <span className="text-xl font-bold">TSP Company</span>
+        </Link>
+
+        <div className="md:hidden flex items-center">
+          <button onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? (
+              <X className="text-2xl" onClick={closeMenu} />
+            ) : (
+              <Menu className="text-2xl" />
+            )}
+          </button>
+        </div>
+      </div>
+
       {/* Mobile menu */}
       <AnimatePresence>
         {menuOpen && (
           <motion.div
-            className="md:hidden fixed top-0 left-0 w-3/4 max-w-xs h-full bg-background shadow-lg z-50"
-            id="mobile-menu"
-            variants={mobileMenuVariants}
-            initial="closed"
-            animate="open"
-            exit="closed"
+            className="fixed inset-0  z-50 md:hidden"
+            onClick={() => servicesOpen && closeMenu()}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
           >
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 border-t border-border mt-16">
-              {navLinks.map((link) =>
-                !link.submenu ? (
-                  <Link
-                    key={link.name}
-                    href={link.href}
-                    className={`block text-foreground hover:text-primary-foreground transition-colors px-3 py-2 rounded-md text-base font-medium ${
-                      pathname === link.href ? "text-primary" : ""
-                    }`}
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    {link.name}
-                  </Link>
-                ) : (
-                  <div key={link.name} className="block">
-                    <button
-                      onClick={() => setServicesOpen((open) => !open)}
-                      className="w-full flex items-center justify-between text-foreground hover:text-primary-foreground transition-colors px-3 py-2 rounded-md text-base font-medium"
+            <motion.div
+              className="fixed inset-y-0 left-0 w-[75%] bg-white dark:bg-slate-800  z-50 overflow-y-auto"
+              variants={mobileMenuVariants}
+              initial="closed"
+              animate="open"
+              exit="closed"
+            >
+              <div className="text-primary font-bold flex justify-between items-center p-4">
+                <h1 className="text-xl font-bold">TSP Company</h1>
+              </div>
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3   ">
+                {navLinks.map((link) =>
+                  !link.submenu ? (
+                    <Link
+                      key={link.name}
+                      href={link.href}
+                      className={`block text-foreground hover:text-primary-foreground transition-colors px-3 py-2 rounded-md text-base font-medium ${
+                        pathname === link.href ? "text-primary" : ""
+                      }`}
+                      onClick={closeMenu}
                     >
                       {link.name}
-                      {/* <svg
-                        className={`w-4 h-4 ml-1 transform transition-transform duration-200 ${
-                          servicesOpen ? "rotate-180" : "rotate-0"
-                        }`}
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
+                    </Link>
+                  ) : (
+                    <div key={link.name} className="block">
+                      <button
+                        onClick={() => setServicesOpen((open) => !open)}
+                        className="w-full flex items-center justify-between text-foreground hover:text-primary-foreground transition-colors px-3 py-2 rounded-md text-base font-medium"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M19 9l-7 7-7-7"
-                        />
-                      </svg> */}
-                      {servicesOpen ? (
-                        <ChevronUp
-                          className={`w-4 h-4 ml-1 transform transition-transform duration-200 ${
-                            servicesOpen ? "rotate-180" : "rotate-0"
-                          }`}
-                        />
-                      ) : (
-                        <ChevronDown
-                          className={`w-4 h-4 ml-1 transform transition-transform duration-200 ${
-                            servicesOpen ? "rotate-180" : "rotate-0"
-                          }`}
-                        />
-                      )}
-                    </button>
-                    {servicesOpen && (
-                      <div className="pl-4">
-                        {link.submenu.map((sublink) => (
-                          <Link
-                            key={sublink.name}
-                            href={sublink.href}
-                            className={`block px-4 py-2 text-sm text-foreground hover:bg-primary/10 hover:text-primary transition-colors ${
-                              pathname === sublink.href ? "text-primary" : ""
+                        {link.name}
+
+                        {servicesOpen ? (
+                          <ChevronUp
+                            className={`w-4 h-4 ml-1 transform transition-transform duration-200 ${
+                              servicesOpen ? "rotate-180" : "rotate-0"
                             }`}
-                            onClick={() => setMenuOpen(false)}
-                          >
-                            {sublink.name}
-                          </Link>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-                )
-              )}
-            </div>
+                          />
+                        ) : (
+                          <ChevronDown
+                            className={`w-4 h-4 ml-1 transform transition-transform duration-200 ${
+                              servicesOpen ? "rotate-180" : "rotate-0"
+                            }`}
+                          />
+                        )}
+                      </button>
+                      {servicesOpen && (
+                        <div className="pl-4">
+                          {link.submenu.map((sublink) => (
+                            <Link
+                              key={sublink.name}
+                              href={sublink.href}
+                              className={`block px-4 py-2 text-sm text-foreground hover:bg-primary/10 hover:text-primary transition-colors ${
+                                pathname === sublink.href ? "text-primary" : ""
+                              }`}
+                              onClick={() => setServicesOpen(false)}
+                            >
+                              {sublink.name}
+                            </Link>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )
+                )}
+              </div>
+            </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
