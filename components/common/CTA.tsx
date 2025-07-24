1f083de5087bc9cd030a6a255c83518a6989ebda
kasahun-welela <kasahunwelela1@gmail.com>
2025-07-24 11:46:31 +0300

    update margin

--- components/common/CTA.tsx
+++ components/common/CTA.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function CTA({ title, description }: { title: string; description: string }) {
   return (
-    <section className="bg-primary py-16 mt-5">
+    <section className="bg-primary py-16 mt-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h2 className="text-3xl font-bold text-white mb-4">{title}</h2>
         <p className="text-xl text-blue-100 mb-8">{description}</p>
